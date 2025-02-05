83b8de21e4f80be7006d508b4266db1a8e721530
Zehra Seren <fatmazehraseren@gmail.com>
2025-02-05 11:02:12 +0300

    Lecture 3 is completed

---
+++ Domain/Entities/Movie.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.Entities
+{
+    public class Movie
+    {
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public string CoverImageUrl { get; set; }
+        public decimal Rating { get; set; }
+        public string Description { get; set; }
+        public int Duration { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string CreatedYear { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ Domain/Entities/Review.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Entities
+{
+    public class Review
+    {
+        public int ReviewId { get; set; }
+        public string ReviewComment { get; set; }
+        public int UserRating { get; set; }
+        public DateTime ReviewDate { get; set; }
+        public bool Status { get; set; }
+    }
+}
