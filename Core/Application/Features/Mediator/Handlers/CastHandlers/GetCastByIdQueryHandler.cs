0eabf75bc8905d36a3947652f9e3b34748568fec
Zehra Seren <fatmazehraseren@gmail.com>
2025-03-13 19:04:21 +0300

    Lecture 18 is completed

---
+++ Core/Application/Features/Mediator/Handlers/CastHandlers/GetCastByIdQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Persistence.Context;
+using Application.Features.Mediator.Results.CastResults;
+using Application.Features.Mediator.Queries.CastQueries;
+
+namespace Application.Features.Mediator.Handlers.CastHandlers;
+
+public class GetCastByIdQueryHandler : IRequestHandler<GetCastByIdQuery, GetCastByIdQueryResult>
+{
+    private readonly MovieContext _context;
+
+    public GetCastByIdQueryHandler(MovieContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<GetCastByIdQueryResult> Handle(GetCastByIdQuery request, CancellationToken cancellationToken)
+    {
+        var value = await _context.Casts.FindAsync(request.Id);
+        return new GetCastByIdQueryResult
+        {
+            Title = value.Title,
+            Name = value.Name,
+            Surname = value.Surname,
+            ImageUrl = value.ImageUrl,
+            Overview = value.Overview,
+            Biography = value.Biography,
+        };
+    }
+}
---
+++ Core/Application/Features/Mediator/Handlers/CastHandlers/GetCastQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using Application.Features.Mediator.Results.CastResults;
+using Application.Features.Mediator.Queries.CastQueries;
+
+namespace Application.Features.Mediator.Handlers.CastHandlers;
+
+public class GetCastQueryHandler : IRequestHandler<GetCastQuery, List<GetCastQueryResult>>
+{
+    private readonly MovieContext _context;
+
+    public GetCastQueryHandler(MovieContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<GetCastQueryResult>> Handle(GetCastQuery request, CancellationToken cancellationToken)
+    {
+        var values = await _context.Casts.ToListAsync();
+        return values.Select(x => new GetCastQueryResult
+        {
+            Title = x.Title,
+            Name = x.Name,
+            Surname = x.Surname,
+            ImageUrl = x.ImageUrl,
+            Overview = x.Overview,
+            Biography = x.Biography,
+        }).ToList();
+    }
+}
