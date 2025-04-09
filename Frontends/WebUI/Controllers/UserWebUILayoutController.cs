a20ff19cb737080f0cdd3bfee03205d0d33982bd
Zehra Seren <fatmazehraseren@gmail.com>
2025-04-09 23:41:51 +0300

    Lecture 23 is completed

---
+++ Frontends/WebUI/Controllers/UserWebUILayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class UserWebUILayoutController : Controller
+    {
+        public IActionResult UILayout()
+        {
+            return View();
+        }
+    }
+}
