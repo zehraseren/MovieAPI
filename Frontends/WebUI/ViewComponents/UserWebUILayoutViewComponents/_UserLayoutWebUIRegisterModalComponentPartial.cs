3e1262110fe2683e8b15a169c3594f0b9679b95c
Zehra Seren <fatmazehraseren@gmail.com>
2025-04-09 23:57:03 +0300

    Lecture 24 is completed

---
+++ Frontends/WebUI/ViewComponents/UserWebUILayoutViewComponents/_UserLayoutWebUIRegisterModalComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.ViewComponents.UserWebUILayoutViewComponents;
+
+public class _UserLayoutWebUIRegisterModalComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
