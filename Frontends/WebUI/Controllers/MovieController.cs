094f8256aadf9be9e35225f9359bcf6445379c0a
Zehra Seren <fatmazehraseren@gmail.com>
2025-04-12 15:03:50 +0300

    Lecture 26 is completed

---
+++ Frontends/WebUI/Controllers/MovieController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers;
+
+public class MovieController : Controller
+{
+    public IActionResult MovieList()
+    {
+        ViewBag.title = "Film Listesi";
+        ViewBag.navbarName = "Ana Sayfa";
+        ViewBag.subtitle = "Tüm Filmler";
+        return View();
+    }
+}
---
+++ Frontends/WebUI/ViewComponents/UserWebUILayoutViewComponents/_UserLayoutWebUIHeroComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.ViewComponents.UserWebUILayoutViewComponents;
+
+public class _UserLayoutWebUIHeroComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
