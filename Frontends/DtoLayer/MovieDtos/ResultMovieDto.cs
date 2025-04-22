b43279f91d93fc4c1a2636ab51a4339bf803106f
Zehra Seren <fatmazehraseren@gmail.com>
2025-04-22 16:54:40 +0300

    Lecture 28 & 29 are completed

---
+++ Frontends/DtoLayer/MovieDtos/ResultMovieDto.cs
@@ -0,0 +1,14 @@
+﻿namespace DtoLayer.MovieDtos;
+
+public class ResultMovieDto
+{
+    public int MovieId { get; set; }
+    public string Title { get; set; }
+    public string CoverImageUrl { get; set; }
+    public decimal Rating { get; set; }
+    public string Description { get; set; }
+    public int Duration { get; set; }
+    public DateTime ReleaseDate { get; set; }
+    public string CreatedYear { get; set; }
+    public bool Status { get; set; }
+}
--- Frontends/WebUI/Controllers/MovieController.cs
+++ Frontends/WebUI/Controllers/MovieController.cs
@@ -1,14 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Newtonsoft.Json;
+using DtoLayer.MovieDtos;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebUI.Controllers;
 
 public class MovieController : Controller
 {
-    public IActionResult MovieList()
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public MovieController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IActionResult> MovieList()
     {
         ViewBag.mainTitle = "FİLM LİSTESİ";
         ViewBag.navbarName = "ANA SAYFA";
         ViewBag.subtitle = "TÜM FİLMLER";
+
+        var client = _httpClientFactory.CreateClient();
+        var response = await client.GetAsync("https://localhost:44361/api/Movies");
+        if (response.IsSuccessStatusCode)
+        {
+            var data = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<List<ResultMovieDto>>(data);
+            return View(result);
+        }
         return View();
     }
 }
