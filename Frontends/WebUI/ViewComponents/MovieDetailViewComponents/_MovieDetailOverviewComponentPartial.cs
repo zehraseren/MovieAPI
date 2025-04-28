33264f8e03b47d4152ccba83123fff2713224d82
Zehra Seren <fatmazehraseren@gmail.com>
2025-04-29 01:32:28 +0300

    Lecture 31 is completed

---
+++ Frontends/WebUI/ViewComponents/MovieDetailViewComponents/_MovieDetailOverviewComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.ViewComponents.MovieDetailViewComponents;
+
+public class _MovieDetailOverviewComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
---
+++ Frontends/WebUI/ViewComponents/MovieDetailViewComponents/_MovieDetailRateComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.ViewComponents.MovieDetailViewComponents;
+
+public class _MovieDetailRateComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
