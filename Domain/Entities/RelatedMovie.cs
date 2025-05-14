7b786f5027a59f1efbfc51441a0e2a33269d0368
Zehra Seren <fatmazehraseren@gmail.com>
2025-05-14 17:55:51 +0300

    Lecture 32 is completed

---
+++ Domain/Entities/RelatedMovie.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Entities;
+
+public class RelatedMovie
+{
+    public int RelatedMovieId { get; set; }
+    public int MovieId { get; set; }
+    public int UserId { get; set; }
+    public bool IsWatch { get; set; }
+}
