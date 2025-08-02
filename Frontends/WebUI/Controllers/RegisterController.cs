08243258adcfd1a711338200aa5f78eaeeedc804
Zehra Seren <fatmazehraseren@gmail.com>
2025-08-02 17:45:44 +0300

    Lecture 37 is completed

--- Frontends/WebUI/Controllers/RegisterController.cs
+++ Frontends/WebUI/Controllers/RegisterController.cs
@@ -1,10 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Text;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc;
 using DtoLayer.UserRegisterDtos;
 
 namespace WebUI.Controllers;
 
 public class RegisterController : Controller
 {
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public RegisterController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
     [HttpGet]
     public IActionResult SignUp()
     {
@@ -12,8 +21,18 @@ public class RegisterController : Controller
     }
 
     [HttpPost]
-    public IActionResult SignUp(CreateUserRegisterDto curdto)
+    public async Task<IActionResult> SignUp(CreateUserRegisterDto curdto)
     {
-        return RedirectToAction("SignIn", "Login");
+        var client = _httpClientFactory.CreateClient();
+        var jsonData = JsonConvert.SerializeObject(curdto);
+        StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+        var response = await client.PostAsync("https://localhost:44361/api/UserRegisters", content);
+        if (response.IsSuccessStatusCode)
+        {
+            return RedirectToAction("SignIn", "Login");
+        }
+
+        ViewBag.error = "Kullanıcı kaydı oluşturulamadı. Lütfen tekrar deneyin.";
+        return View();
     }
 }
