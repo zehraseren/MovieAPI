ea84b66c64a0b66a098ef3e268eec7b3f36bd0d6
Zehra Seren <fatmazehraseren@gmail.com>
2025-08-02 17:32:37 +0300

    Lecture 36 is completed

---
+++ Frontends/DtoLayer/UserRegisterDtos/CreateUserRegisterDto.cs
@@ -0,0 +1,10 @@
+﻿namespace DtoLayer.UserRegisterDtos;
+
+public class CreateUserRegisterDto
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Email { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
---
+++ Frontends/WebUI/Controllers/RegisterController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DtoLayer.UserRegisterDtos;
+
+namespace WebUI.Controllers;
+
+public class RegisterController : Controller
+{
+    [HttpGet]
+    public IActionResult SignUp()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult SignUp(CreateUserRegisterDto curdto)
+    {
+        return RedirectToAction("SignIn", "Login");
+    }
+}
